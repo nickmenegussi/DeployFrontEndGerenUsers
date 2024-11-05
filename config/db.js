879d48efb84c0098cbb5114067bb2bf12b7fdf82
Nicolas menegussi ramos <106559733+nickmenegussi@users.noreply.github.com>
2024-11-05 11:40:20 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const conexao = mysql.createConnection({
-    host: 'sql10.freemysqlhosting.net', // Database host
-    user: 'sql10740786', // Database Username
-    password: 'Fm6XknD2vU', // Database Password
-    database: 'sql10740786' // Database Name
+    host: 'sql5.freemysqlhosting.net', // Database host
+    user: 'sql5742779', // Database Username
+    password: 'uJCe9SrXs7', // Database Password
+    database: 'sql5742779' // Database Name
 });
 
 conexao.connect(err => {
